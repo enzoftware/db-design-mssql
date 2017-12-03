eaae1899b5173d956d349bb5f39e1efaf5d5b949
enzoftware <lizama.enzo@gmail.com>
2017-12-03 00:36:10 -0500

    connection with socket.io

--- server.js
+++ server.js
@@ -4,4 +4,13 @@ var server = app.listen(3000);
 
 app.use(expreess.static('public'));
 
+var socket = require('socket.io');
+var io = socket(server);
+
+io.sockets.on('connection', newConnection);
+
+function newConnection(socket){
+    console.log('new connection ' + socket.id);
+}
+
 console.log("My socket server is running");
