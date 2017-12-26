55e4d86041c85648a6aac4cfe6c994310de47d57
enzoftware <lizama.enzo@gmail.com>
2017-12-25 20:16:27 -0500

    Messanging working

--- public/sketch.js
+++ public/sketch.js
@@ -1,11 +1,28 @@
 var socket;
-
 function setup(){
     createCanvas(300,300);
     background(51);
     socket = io.connect('http://localhost:3000');
+    var messageForm = $('#send-message');
+    var messageBox = $('#message');
+    var chat = $('#chat');
+
+    messageForm.submit(function(e){
+      //alert("hello");
+      e.preventDefault();
+      socket.emit('send_message',messageBox.val());
+      console.log(messageBox.val());
+      messageBox.val('');
+    });
 
     socket.on('mouse',newDrawing);
+
+    socket.on('new_message', reciveMessage);
+
+    function reciveMessage(data){
+      console.log(data);
+      chat.append(data + '<br/>');
+    }
 
     function newDrawing(data){
         noStroke();
@@ -14,8 +31,8 @@ function setup(){
     }
 }
 
-function draw(){    
-    
+function draw(){
+
 }
 
 function mouseDragged(){
@@ -24,7 +41,7 @@ function mouseDragged(){
     var data = {
         x : mouseX,
         y : mouseY
-    }; 
+    };
 
     socket.emit('mouse',data);
 
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var app = express();
 var server = app.listen(3000);
+var socket = require('socket.io');
+var io = socket(server);
 
 app.use(express.static('public'));
 
-var socket = require('socket.io');
-var io = socket(server);
 
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket){
     console.log('new connection ' + socket.id);
-    socket.on('mouse',mouseMessage)
+    socket.on('mouse',mouseMessage);
+    socket.on('send_message',messagePrint);
+
+    function messagePrint(data){
+      io.sockets.emit('new_message',data);
+    }
 
     function mouseMessage(data){
         socket.broadcast.emit('mouse',data);
